a3487d6e9c4490a4759c4a2313480d0b3d551aa3
Abhishek810 <abhishek96.r@gmail.com>
2017-02-02 18:11:29 +0530

    bootarray.c

---
+++ bootarray.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,size,num,count=0;
+    //Size of array
+    scanf("%d",&size);
+    //Elements of array
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    //Number to be searched
+    scanf("%d",&num);
+    for(i=0;i<size;i++)
+    {
+        if(a[i]==num)
+        {
+            printf("%d\t",i);
+            count++;
+        }
+    }
+    if(count==0)
+    {
+        printf("\nNumber not present");
+    }
+    return 0;
+}
